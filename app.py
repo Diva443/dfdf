95f9bd60c31f1fe8ff25c29831dd36c2ebb72628
Diva443 <115131951+Diva443@users.noreply.github.com>
2023-10-26 20:30:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import streamlit as st
 import tensorflow as tf
 import numpy as np
@@ -6,12 +5,8 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-# Set the working directory to the model directory
-# model_dir = 'model'  # Use a relative path
-# os.chdir(model_dir)
-
-# Load the trained model
-model = load_model('model1.hdf5')  # Use a relative path
+# Load the trained model (assuming it's in the same directory)
+model = load_model('model1.hdf5')
 
 # Define the classes
 class_names = ['Real', 'Fake']
@@ -36,70 +31,7 @@ st.set_page_config(page_title="Real vs. Fake Image Classifier")
 # Custom CSS
 st.markdown(
     """
-    <style>
-        .stApp {
-            text-align: center;
-        }
-
-        .stApp header {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .stApp img {
-            max-width: 80%;
-            margin: 20px auto;
-        }
-
-        .stButton > button {
-            background-color: #FFA500;
-            color: white;
-            font-size: 16px;
-            font-weight: bold;
-            padding: 10px 20px;
-        }
-
-        .stMarkdown {
-            font-family: 'YourUniqueFont', sans-serif;
-            font-size: 24px;
-            font-weight: bold;
-            color: #FFA500;
-            margin-top: 20px;
-            animation-name: fade-in;
-            animation-duration: 1s;
-        }
-
-        .prediction-results {
-            text-align: left;
-            margin-top: 20px;
-            animation-name: fade-in;
-            animation-duration: 1s;
-        }
-
-        .prediction-label {
-            font-size: 24px;
-            font-weight: bold;
-            animation-name: fade-in;
-            animation-duration: 1s;
-        }
-
-        .probability {
-            font-size: 20px;
-            animation-name: fade-in;
-            animation-duration: 1s;
-        }
-
-        /* Fade-in animation */
-        @keyframes fade-in {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-    </style>
+    <!-- Your CSS styles here -->
     """,
     unsafe_allow_html=True,
 )
@@ -129,7 +61,7 @@ if uploaded_image is not None:
             st.markdown(f"<div class='probability'>{confidence:.2f} %</div>", unsafe_allow_html=True)
             st.progress(confidence / 100)  # Convert to [0, 1] range
         else:
-            st.markdown("<div class='prediction-label'>FAKE</div>", unsafe_allow_html=True)
+            st.markdown("<div class 'prediction-label'>FAKE</div>", unsafe_allow_html=True)
             st.markdown(f"<div class='probability'>{(100 - confidence):.2f} %</div>", unsafe_allow_html=True)
             st.progress((100 - confidence) / 100)  # Convert to [0, 1] range
 
